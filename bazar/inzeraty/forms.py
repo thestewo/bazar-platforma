0d57151343eb6adedc0193253357df3651c202dc
thestewo <dzurjakub@gmail.com>
2026-04-11 20:25:39 +0200

    pridana excelentná hudba

--- bazar/inzeraty/forms.py
+++ bazar/inzeraty/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 from .models import Inzerat
 
+# Hľadaj v inzeraty/forms.py
 class InzeratForm(forms.ModelForm):
     class Meta:
         model = Inzerat
         fields = ['nazov', 'popis', 'cena', 'kategoria', 'typ', 'obrazok']
+        labels = {
+            'nazov': 'Názov inzerátu',
+            'popis': 'Podrobný popis',
+            'cena': 'Cena (€)',
+            'kategoria': 'Kategória',
+            'typ': 'Typ inzerátu',
+            'obrazok': 'Fotografia produktu',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control bg-dark text-white border-secondary'})
